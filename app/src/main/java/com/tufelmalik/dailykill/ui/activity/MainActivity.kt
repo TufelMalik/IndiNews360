d4985ce6df9d766a23067340016e285c199d8930
TufelMalik <mtufel21@gmail.com>
2023-09-15 11:03:38 +0530

    college commit -> BottomNav updated .

--- app/src/main/java/com/tufelmalik/dailykill/ui/activity/MainActivity.kt
+++ app/src/main/java/com/tufelmalik/dailykill/ui/activity/MainActivity.kt
@@ -8,6 +8,7 @@ import com.tufelmalik.dailykill.R
 import com.tufelmalik.dailykill.databinding.ActivityMainBinding
 import com.tufelmalik.dailykill.ui.fragments.NewsFragment
 import com.tufelmalik.dailykill.ui.fragments.WorldNewsFragment
+import nl.joery.animatedbottombar.AnimatedBottomBar
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -24,27 +25,38 @@ class MainActivity : AppCompatActivity() {
         binding.mainHeader.text = getString(R.string.app_name)
         replaceFragment(NewsFragment())
 
+        setBottomNav()
 
 
 
-        binding.bottomNav.setOnItemSelectedListener { menuItem ->
-            when (menuItem.itemId) {
-                R.id.idNews_nav -> {
-                    replaceFragment(NewsFragment())
+    }
 
-                    binding.mainHeader.text = "DailyHunt"
+    private fun setBottomNav(){
+        binding.bottomNav.setOnTabInterceptListener(object : AnimatedBottomBar.OnTabInterceptListener {
+            override fun onTabIntercepted(
+                lastIndex: Int,
+                lastTab: AnimatedBottomBar.Tab?,
+                newIndex: Int,
+                newTab: AnimatedBottomBar.Tab
+            ): Boolean {
+                when (newTab.id) {
+                    R.id.idNews_nav -> {
+                        replaceFragment(NewsFragment())
+                        binding.mainHeader.text = "DailyHunt"
+                    }
+                    R.id.idWorld_nav -> {
+                        binding.mainHeader.text = "World News"
+                        replaceFragment(WorldNewsFragment())
+                    }
+                    R.id.idWether_nav -> {
+                        binding.mainHeader.text = "Weather Updates"
+                        startActivity(Intent(this@MainActivity,WeatherActivity::class.java))
+                    }
+                    else->false
                 }
-                R.id.idWorld_nav -> {
-                    binding.mainHeader.text = "World News"
-                    replaceFragment(WorldNewsFragment())
-                }
-                R.id.idWether_nav -> {
-                    binding.mainHeader.text = "Weather Updates"
-                    startActivity(Intent(this@MainActivity,WeatherActivity::class.java))
-                }
+                return true
             }
-            true
-        }
+        })
     }
 
     private fun replaceFragment(fragment: Fragment) {
--- app/src/main/java/com/tufelmalik/dailykill/ui/activity/NewsActivity.kt
+++ app/src/main/java/com/tufelmalik/dailykill/ui/activity/NewsActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.tufelmalik.dailykill.R
-import com.tufelmalik.dailykill.data.classes.Constants.setDate2Days
 import com.tufelmalik.dailykill.databinding.ActivityNewsBinding
 
 
@@ -42,7 +41,7 @@ class NewsActivity : AppCompatActivity()  {
                 .thumbnail(Glide.with(this@NewsActivity).load(R.drawable.loading))
                 .into(binding.imgNewsNewsActivity)
             binding.txtTitleNewsActivity.text = newsTitle
-            setDate2Days(binding.txtPublishedAtNewsActivity,newsPublishDate!!)
+            binding.txtPublishedAtNewsActivity.text =newsPublishDate
             binding.txtDescriptionNewsActivity.text = newsDes
         }
 }
